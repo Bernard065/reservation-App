2aa6729ca66c95707d7946f9725845270d2fa007
Bernard065 <bebenibernard@gmail.com>
2023-03-28 15:35:09 +0300

    running migrations and seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,7 +6,6 @@
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
 
-
 puts "🦸‍♀️ Seeding powers..."
 # create users
 10.times do
