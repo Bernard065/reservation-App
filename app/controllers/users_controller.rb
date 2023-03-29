84cafc6ffeb1405e00afa0ffa4eec154218b02b5
Bernard065 <bebenibernard@gmail.com>
2023-03-29 04:23:56 +0300

    signup

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,6 @@
 class UsersController < ApplicationController
+    rescue_from ActiveRecord::RecordInvalid, with: :render_unprocessable_entity_response
+
 
     def show
         @user = User.find_by(id: session[:user_id])
@@ -8,4 +10,25 @@ class UsersController < ApplicationController
             render json: { error: "Not authorized" }, status: :unauthorized
         end
     end
+
+    def create
+        @user = User.create!(user_params)
+        if @user.valid?
+            session[:user_id] = @user.id
+            render json: @user, status: :created
+        else
+            render json: { errors: @user.errors.full_messages }, status: :unprocessable_entity
+        end
+    end
+
+    private
+
+    def user_params
+        params.permit(:username, :password, :password_confirmation)
+    end
+
+    def render_unprocessable_entity_response(invalid)
+        render json: { errors: invalid.record.errors.full_messages }, status: :unprocessable_entity
+    end
+
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -2,6 +2,8 @@ class User < ApplicationRecord
     has_many :reservations
     has_many :rooms, through: :reservations
 
+    has_secure_password
+
     validates :username, presence: true, uniqueness: true
     validates :email, presence: true, uniqueness: true, format: { with: URI::MailTo::EMAIL_REGEXP }
     validates :last_name, presence: true
