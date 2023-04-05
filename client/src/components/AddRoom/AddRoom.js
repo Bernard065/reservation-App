8ce7b6f6a94b62fccc3651eba0af0132fc8a8728
Bernard065 <bebenibernard@gmail.com>
2023-04-05 07:06:47 +0300

    autorizing API endpoints

--- client/src/components/AddRoom/AddRoom.js
+++ client/src/components/AddRoom/AddRoom.js
@@ -14,7 +14,7 @@ const AddRoom = () => {
         extras: ''
     })
 
-    const [errors, setErrors] = useState({});
+    const [errors, setErrors] = useState([]);
 
     const handleChange = (event) => {
         const target = event.target;
@@ -27,154 +27,190 @@ const AddRoom = () => {
         });
     };
     
-    
-
     const handleSubmit = async (event) => {
         event.preventDefault();
-        const response = await fetch('rooms', {
-          method: 'POST',
-          headers: {
-            'Content-Type': 'application/json'
-          },
-          body: JSON.stringify(formData)
-        });
-        const data = await response.json();
-        if (response.ok) {
-            alert('Room added successfully!');
-        } else {
-          // handle error
-          setErrors(data.errors);
+        try {
+            const response = await fetch('rooms', {
+              method: 'POST',
+              headers: {
+                'Content-Type': 'application/json'
+              },
+              credentials: 'include',
+              body: JSON.stringify(formData)
+            });
+            const data = await response.json();
+            if (response.ok) {
+                alert('Room added successfully!');
+            } else {
+                setErrors(data.errors);
+            }
+        } catch (error) {
+            setErrors(['An error occurred. Please try again.']);
         }
     };
     
 
-  return (
-    <div className='add-room'>
-        <h1>Add Room</h1>
-        {errors.length > 0 && (
-            <div>
-                <h2>Errors:</h2>
-                <ul>
-                    {errors.map((error, index) => (
-                    <li key={index}>{error}</li>
-                    ))}
-                </ul>
-            </div>
-        )}
-        <form onSubmit={handleSubmit}>
-            <div className='form-group'>
-                <label htmlFor="name">Name:</label>
-                <input
-                    type="text"
-                    id="name"
-                    name="name"
-                    value={formData.name}
-                    onChange={handleChange}
-                r   equired
-                />
-            </div>
-            <div>
-                <label htmlFor="category">Category:</label>
-                <input
-                    type="text"
-                    id="category"
-                    name="category"
-                    value={formData.category}
-                    onChange={handleChange}
-                    required
-                />
-            </div>
-            <div>
-                <label htmlFor="price">Price:</label>
-                <input
-                    type="number"
-                    id="price"
-                    name="price"
-                    value={formData.price}
-                    onChange={handleChange}
-                    required
-                    min="0"
-                />
-            </div>
-            <div>
-                <label htmlFor="size">Size:</label>
-                <input
-                    type="number"
-                    id="size"
-                    name="size"
-                    value={formData.size}
-                    onChange={handleChange}
-                    required
-                    min="0"
-                />
-            </div>
-            <div>
-                <label htmlFor="capacity">Capacity:</label>
-                <input
-                    type="number"
-                    id="capacity"
-                    name="capacity"
-                    value={formData.capacity}
-                    onChange={handleChange}
-                    required
-                    min="0"
-                />
-            </div>
-            <div>
-                <label htmlFor="breakfast">Breakfast:</label>
-                <input
-                    type="checkbox"
-                    id="breakfast"
-                    name="breakfast"
-                    checked={formData.breakfast}
-                    onChange={handleChange}
-                />
-            </div>
-            <div>
-                <label htmlFor="featured">Featured:</label>
-                <input
-                    type="checkbox"
-                    id="featured"
-                    name="featured"
-                    checked={formData.featured}
-                    onChange={handleChange}
-                />
-            </div>
-            <div>
-                <label htmlFor="description">Description:</label>
-                <textarea
-                    id="description"
-                    name="description"
-                    value={formData.description}
-                    onChange={handleChange}
-                    required>    
-                </textarea>
-            </div>
-            <div>
-                <label htmlFor="extras">Extras:</label>
-                <textarea
-                    id="extras"
-                    name="extras"
-                    value={formData.extras}
-                    onChange={handleChange}>
-                </textarea>
-            </div>
-            <div>
-                <label htmlFor="img_url">Image URL:</label>
-                <input
-                    type="text"
-                    id="img_url"
-                    name="img_url"
-                    value={formData.img_url}
-                    onChange={handleChange}
-                />
-            </div>
-            <div>
-                <button type="submit">Add</button>
-            </div>
-        </form>
-    </div>
-  )
+    return (
+        <div className='add-room'>
+            <h1>Add Room</h1>
+            {errors.length > 0 && (
+                <div className='errors'>
+                    <h2>Errors:</h2>
+                    <ul>
+                        {errors.map((error, index) => (
+                        <li key={index}>{error}</li>
+                        ))}
+                    </ul>
+                </div>
+            )}
+            <form onSubmit={handleSubmit}>
+                <div className='form-group'>
+                    <label htmlFor="name">Name:</label>
+                    <input
+                        type="text"
+                        id="name"
+                        name="name"
+                        value={formData.name}
+                        onChange={handleChange}
+                        required
+                        className='input-field'
+                    />
+                </div>
+                <div className='form-group'>
+                    <label htmlFor="category">Category:</label>
+                    <input
+                        type="text"
+                        id="category"
+                        name="category"
+                        value={formData.category}
+                        onChange={handleChange}
+                        required
+                        className='input-field'
+                    />
+                </div>
+                <div className='form-group'> 
+                    <label htmlFor="price">Price:</label>
+                    <input
+                        type="number"
+                        id="price"
+                        name="price"
+                        value={formData.price}
+                        onChange={handleChange}
+                        required
+                        min="0"
+                        className='input-field'
+                    />
+                </div>
+                <div className='form-group'>
+                    <label htmlFor="size">Size:</label>
+                    <input
+                        type="number"
+                        id="size"
+                        name="size"
+                        value={formData.size}
+                        onChange={handleChange}
+                        required
+                        min="0"
+                        className='input-field'
+                    />
+                </div>
+                <div className='form-group'>
+                    <label htmlFor="capacity">Capacity:</label>
+                    <input
+                        type="number"
+                        id="capacity"
+                        name="capacity"
+                        value={formData.capacity}
+                        onChange={handleChange}
+                        required
+                        min="0"
+                        className='input-field'
+                    />
+                    <div className='form-group-helper'>
+                        <small>Enter the maximum number of people this room can accommodate.</small>
+                    </div>
+                </div>
+                <div className='form-group'>
+                    <label htmlFor='description'>Description:</label>
+                    <textarea
+                        id='description'
+                        name='description'
+                        value={formData.description}
+                        onChange={handleChange}
+                        required
+                        className='input-field'
+                    >
+                        
+                    </textarea>
+                    <div className='form-group-helper'>
+                        <small>Provide a brief description of the room.</small>
+                    </div>
+                </div>
+                <div className="form-group">
+                    <label htmlFor="extras" className="label-field">Extras:</label>
+                    <textarea
+                        id="extras"
+                        name="extras"
+                        value={formData.extras}
+                        onChange={handleChange}
+                        className="input-field textarea-field"
+                    >
+                    </textarea>
+                    <div className="form-group-helper">
+                        <small>Enter comma-separated list of additional features and amenities of the room (e.g. Wi-Fi, TV, air conditioning).</small>
+                    </div>
+                </div>
+                <div className="form-group">
+                    <label htmlFor="img_url" className="label-field">Image URL:</label>
+                    <input
+                        type="text"
+                        id="img_url"
+                        name="img_url"
+                        value={formData.img_url}
+                        onChange={handleChange}
+                        className="input-field"
+                    />
+                    <div className="form-group-helper">
+                        <small>Enter the URL of an image for this room.</small>
+                    </div>
+                </div>
+                <div className="form-group">
+                    <label htmlFor="breakfast" className="checkbox-label">
+                    <input
+                        type="checkbox"
+                        id="breakfast"
+                        name="breakfast"
+                        checked={formData.breakfast}
+                        onChange={handleChange}
+                        className='checkbox-field'
+                    />
+                    <span className="label-text">Breakfast</span>
+                    </label>
+                    <div className="form-group-helper">
+                        <small>Select if breakfast is included with this room.</small>
+                    </div>
+                </div>
+                <div className='form-group'>
+                    <label htmlFor="featured" className="checkbox-label">
+                    <input
+                        type="checkbox"
+                        id="featured"
+                        name="featured"
+                        checked={formData.featured}
+                        onChange={handleChange}
+                        className='checkbox-field'
+                    />
+                    <span className="label-text">Featured</span>
+                    </label>
+                    <div className="form-group-helper">
+                        <small>Select if this room should be featured on the home page.</small>
+                    </div>
+                </div>
+                <div>
+                    <button type='submit' className='submit-button'>Add</button>
+                </div>
+            </form>
+        </div>
+    )
 }
-
-export default AddRoom
+export default AddRoom;                                  
--- client/src/components/AdminDashboard/AdminDashboard.js
+++ client/src/components/AdminDashboard/AdminDashboard.js
@@ -1,45 +1,59 @@
-import { useState, useEffect } from 'react';
+import React, { useState, useEffect } from 'react';
 import AddRoom from '../AddRoom/AddRoom';
 import AllReservations from '../AllReservations/AllReservations';
 import './admin.css';
 
-function AdminDashboard() {
+function FetchAllUsers() {
   const [users, setUsers] = useState([]);
+  const [error, setError] = useState('');
 
   useEffect(() => {
-    fetch('/users')
-      .then(response => response.json())
+    fetch('/users', {
+      credentials: 'include'
+    })
+      .then(response => {
+        if (response.ok) {
+          return response.json();
+        } else {
+          throw new Error('Something went wrong while fetching users.');
+        }
+      })
       .then(data => setUsers(data))
-      .catch(error => console.error(error));
+      .catch(error => setError(error.message));
   }, []);
 
   return (
     <div className="container">
       <h1 className="title">All Users</h1>
-      <table className="table">
-        <thead>
-          <tr>
-            <th>Username</th>
-            <th>Email</th>
-            <th>First Name</th>
-            <th>Last Name</th>
-          </tr>
-        </thead>
-        <tbody>
-          {users.map(user => (
-            <tr key={user.id}>
-              <td>{user.username}</td>
-              <td>{user.email}</td>
-              <td>{user.first_name}</td>
-              <td>{user.last_name}</td>
-            </tr>
-          ))}
-        </tbody>
-      </table>
-      <AddRoom />
-      <AllReservations />
+      {error && <p className="error">{error}</p>}
+      {!error && (
+        <>
+          <table className="table">
+            <thead>
+              <tr>
+                <th>Username</th>
+                <th>Email</th>
+                <th>First Name</th>
+                <th>Last Name</th>
+              </tr>
+            </thead>
+            <tbody>
+              {users.map(user => (
+                <tr key={user.id}>
+                  <td>{user.username}</td>
+                  <td>{user.email}</td>
+                  <td>{user.first_name}</td>
+                  <td>{user.last_name}</td>
+                </tr>
+              ))}
+            </tbody>
+          </table>
+          <AddRoom />
+          <AllReservations />
+        </>
+      )}
     </div>
   );
 }
 
-export default AdminDashboard;
+export default FetchAllUsers;
